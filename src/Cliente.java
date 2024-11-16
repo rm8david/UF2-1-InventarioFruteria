1024279447a2f6af5b3cb2aef01f83974182fb79
David RM <rm8david@gmail.com>
2024-11-16 16:37:39 +0100

    Comentarios

--- src/Cliente.java
+++ src/Cliente.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class Cliente {
     public static void main(String[] args) {
         try {
+            //conectamos al servidor con al ip, en este caso la ip local y el puerto que hemos puesto en el servidor
             Socket socket = new Socket("127.0.0.1",1234);
+            //creamos los canales de entrada y salida de datos, al igual que en el servidor
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             DataInputStream in = new DataInputStream(socket.getInputStream());
             String opcion= "";
@@ -15,14 +17,16 @@ public class Cliente {
             do{
                 System.out.println("Ingrese el codigo de fruta, escriba FIN para terminar");
                 System.out.println("Opciones disponibles: PL, PC, PN, BN, TP, TR, UN, UB, PT, CR, MR, MA");
+                //pedimos una clave de dos caracteres por consola y se la enviamos al servidor
                 opcion = sc.next();
                 out.writeUTF(opcion);
-                //
                 out.flush();
+                //recobimos por el in ( DataInputStream ) lo que nos responde el servidor
                 String producto = in.readUTF();
                 System.out.println(producto);
+                //SI la opcion escogida es FIN, salimos y desconectamos
             }while(!opcion.equals("FIN"));
-
+            socket.close();
         } catch (IOException | NullPointerException e) {
                System.out.println(e.getMessage());
         }
--- src/Servidor.java
+++ src/Servidor.java
@@ -11,7 +11,9 @@ import java.util.TreeMap;
 
 public class Servidor {
     public static void main(String[] args) {
+        // Usamos el map este nuevo que nos han dicho, por lo visto es de tipo clave:valor
         TreeMap<String, Producto> productos = new TreeMap<>();
+        //añadimos como lo dice el ejercicio todos estos valores, he puesto tipo double en lugar de float
         productos.put("PL",new Producto("Peras limoneras", 14, 5));
         productos.put("PC",new Producto("Peras conferencia", 12, 7));
         productos.put("PN",new Producto("Plátano canario", 5, 2.5));
@@ -24,33 +26,40 @@ public class Servidor {
         productos.put("CR",new Producto("Ciruelas rojas", 10, 2.8));
         productos.put("MR",new Producto("Melocotones rojos", 3, 2.5));
         productos.put("MA",new Producto("Melocotones amarillos", 4, 3.2));
+
         String opcion = "";
         ServerSocket serverSocket = null;
         try {
+            //Iniciamos el Serversocket aceptando la conexion del cliente
             serverSocket = new ServerSocket(1234);
             Socket socket = serverSocket.accept();
             System.out.println("Servidor conectado, esperando entrada del cliente...");
+            //Creamos los canales de entrada y salida de datos
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            //creamos un producto vacio ya que vamos a trabajar con esta clase
             Producto producto;
+            //while true es infinito, pero tenemos la opcion de romper el bucle si recibimos el String "TRUE" desde el cliente
             while(true){
                 opcion = dataInputStream.readUTF();
                 if(opcion.equals("FIN")){
                     System.out.println("El cliente ha finalizado la conexion");
                     break;
                 }
+                //productos.get recorre la lista de MapTree y recobe el Producto que coincida con la clave opcion
                 producto = productos.get(opcion);
+                //SI no hay coincidencia mandamos este mensaje
                 if(producto==null){
                     dataOutputStream.writeUTF("No se encontro el producto");
                     dataOutputStream.flush();
                 }else{
+                    //Si existe coincidencia mandamos el mensaje producto.toString (es que coincide un producto con una clave)
                     System.out.println("El cliente ha buscado el producto: "+producto.getNombre());
                     dataOutputStream.writeUTF(producto.toString());
                     dataOutputStream.flush();
                 }
             }
         } catch (IOException e) {
-
             System.out.println("Error al iniciar el servidor");
         }
     }
